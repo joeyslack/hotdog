306268277e7525ce9787c009e1376267373852b1
Joseph Slack <josephdslack@gmail.com>
2024-04-03 13:21:50 -0700

    Add data samples for list

---
+++ yelp.js
@@ -0,0 +1,13 @@
+const search = (params) => {
+  return fetch(`https://api.yelp.com/v3/businesses/search?latitude=${params.latitude}&longitude=${params.longitude}&sort_by=best_match&limit=20&categories=hotdog,hotdogs`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      'Authorization': `Bearer ${ process.env.EXPO_PUBLIC_YELP_API_KEY }`,
+      'Accept': 'application/json',
+    },
+  }).then(response => response.json())
+  .then(data => data);
+}
+
+export default search;
