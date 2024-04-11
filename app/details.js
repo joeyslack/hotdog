a6fa2329b102439879e84bb1e96c81c813bcb629
Joseph Slack <josephdslack@gmail.com>
2024-04-11 15:27:25 -0700

    new layout for yelp

--- app/details.js
+++ app/details.js
@@ -6,8 +6,15 @@ import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
 import { Colors, BorderRadiuses, View, Image, ListItem, Text, Card } from 'react-native-ui-lib';
 import search from '../yelp';
 
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://kwlymkxyljhhouzeaocy.supabase.co'
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+
 export default function Details() {
-  // const [value, onChangeText] = React.useState(route.params.title);
   const navigation = useNavigation();
   const params = useLocalSearchParams();
 
@@ -18,35 +25,34 @@ export default function Details() {
    });
   }, [navigation]);
 
-  const sample = require('../sample.yelp.response.json');
+  // const sample = require('../sample.yelp.response.json');
+  // const [location, setLocation] = useState(null);
+  // const [errorMsg, setErrorMsg] = useState(null);
+  // const [placesList, setPlacesList] = useState([]);
 
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [placesList, setPlacesList] = useState([]);
+  // // Location specific stuff...
+  // useEffect(() => {
+  //   (async () => {
+  //     let { status } = await Location.requestForegroundPermissionsAsync();
+  //     if (status !== 'granted') {
+  //       setErrorMsg('Permission to access location was denied');
+  //       return;
+  //     }
 
-  // Location specific stuff...
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      console.log('location: ', location);
+  //     let location = await Location.getCurrentPositionAsync({});
+  //     console.log('location: ', location);
       
-      // setLocation(location);
-      // // DO YELP LOOKUP.
-      // search({
-      //   latitude: location.coords.latitude,
-      //   longitude: location.coords.longitude,
-      // }).then(data => {
-      //   setPlacesList(data.businesses);
-      // });
+  //     // setLocation(location);
+  //     // // DO YELP LOOKUP.
+  //     // search({
+  //     //   latitude: location.coords.latitude,
+  //     //   longitude: location.coords.longitude,
+  //     // }).then(data => {
+  //     //   setPlacesList(data.businesses);
+  //     // });
       
-    })();
-  }, []);
+  //   })();
+  // }, []);
 
 
   // let text = 'Waiting..';
@@ -58,34 +64,14 @@ export default function Details() {
   //   // console.log('LOCATION_DUMP: ', location)
   // }
   const defaultImage = require('../assets/default.jpeg');
-  const generateFlatList = (data) => {
-   
-    // let output = <FlatList contentContainerStyle={{ paddingVertical: 0, width: '100%' }} data={data} keyExtractor={item => item.id} renderItem={ ({item}) =>
-    //     <Card enableShadow={true} row flex marginV-5 onPress={() => console.log('pressed')} style={{ width: '100%', padding: 10 }}>
-    //       {/* <Card.Image source={{uri: item.image_url}} height={115} /> */}
-    //       <Card.Section imageSource={ item.image_url ? {uri: item.image_url} : defaultImage } flexS imageStyle={{height: '100%', width: 100 }}/>
-    //       <Card.Section
-    //         content={[
-    //           {text: item.name, text70H: true, $textDefault: true},
-    //           {text: item.location.display_address.join(',\n'), text80: true, grey30: true},
-    //           {text: (Math.round(item.distance / 1609 * 100) / 100) + " mi", style: {position: 'absolute', right: 5, bottom: 7 }, text100: true, grey30: true }
-    //         ]}
-    //         contentStyle={{alignItems: 'left', paddingVertical: 5}}
-    //         flexG
-    //         marginL-10
-    //         />
-    //     </Card>
-    // } />
-
-    return <View><Text text50H marginL-10 marginB-10>DETAILS VIEW</Text></View>
-  } // generateFlastList()
+  
 
   return (
     <View style={styles.container}>
       <SafeAreaView>
         <View><Text text50H marginL-10 marginB-10>DETAILS VIEW</Text></View>
       </SafeAreaView>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -94,7 +80,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // backgroundColor: '#e00043',
-    backgroundColor: '#e00043',
+    backgroundColor: '#002D72',
     alignItems: 'center',
     justifyContent: 'center',
     color: '#fff',
--- app/index.js
+++ app/index.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import * as Location from 'expo-location';
-import { router } from 'expo-router';
+import { LinearGradient } from 'expo-linear-gradient';
+import { router, Stack, useNavigation } from 'expo-router';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
-import { Colors, BorderRadiuses, View, Image, ListItem, Text, Card } from 'react-native-ui-lib';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Colors, BorderRadiuses, View, Image, Icon, ListItem, Text, Card, FloatingButton, Assets } from 'react-native-ui-lib';
+import * as Animatable from 'react-native-animatable';
+import { BlurView } from 'expo-blur';
 import search from '../yelp';
 
-export default function Page() {
+export default function Home() {
+  const insets = useSafeAreaInsets();
   const sample = require('../sample.yelp.response.json');
 
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [placesList, setPlacesList] = useState([]);
+  const [placesList, setPlacesList] = useState(sample.businesses); // use smaple for testing, use [] for default or storage
 
+  // StatusBar.call()
   // Location specific stuff...
   useEffect(() => {
     (async () => {
@@ -22,20 +28,20 @@ export default function Page() {
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
+      // let location = await Location.getCurrentPositionAsync({});
       console.log('location: ', location);
-      setLocation(location);
-      // DO YELP LOOKUP.
-      search({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      }).then(data => {
-        setPlacesList(data.businesses);
-      });
+      console.log('places: ', placesList);
+      // setLocation(location);
+      // // DO YELP LOOKUP.
+      // search({
+      //   latitude: location.coords.latitude,
+      //   longitude: location.coords.longitude,
+      // }).then(data => {
+      //   setPlacesList(data.businesses);
+      // });
       
     })();
   }, []);
-
 
   // let text = 'Waiting..';
   // if (errorMsg) {
@@ -45,35 +51,56 @@ export default function Page() {
   //   text = JSON.stringify(location);
   //   // console.log('LOCATION_DUMP: ', location)
   // }
+
   const defaultImage = require('../assets/default.jpeg');
   const generateFlatList = (data) => {
-   
-    let output = <FlatList contentContainerStyle={{ paddingVertical: 0, width: '100%' }} data={data} keyExtractor={item => item.id} renderItem={ ({item}) =>
-        <Card enableShadow={true} row flex marginV-5 onPress={() => router.navigate({pathname: '/details', params: {title: item.name}})} style={{ width: '100%', padding: 10 }}>
+    let output = <FlatList flexG contentContainerStyle={{ paddingVertical: 20, paddingHorizontal: 10 }} data={data} keyExtractor={item => item.id} renderItem={ ({item}) =>
+        <Card row flex marginV-5 enableBlur={false} selected={false} activeOpacity={1} enableShadow={false} onPress={() => router.navigate({pathname: '/details', params: item })} style={{ width: '100%', padding: 10, backgroundColor: '#fff'}}>
           {/* <Card.Image source={{uri: item.image_url}} height={115} /> */}
           <Card.Section imageSource={ item.image_url ? {uri: item.image_url} : defaultImage } flexS imageStyle={{height: '100%', width: 100 }}/>
           <Card.Section
             content={[
-              {text: item.name, text70H: true, $textDefault: true},
-              {text: item.location.display_address.join(',\n'), text80: true, grey30: true},
-              {text: (Math.round(item.distance / 1609 * 100) / 100) + " mi", style: {position: 'absolute', right: 5, bottom: 7 }, text100: true, grey30: true }
+              {text: item.name, text70H: true, $textDefault: true, color: '#FF5910'},
+              {text: item.location.display_address.join(',\n'), text80: true, color: '#002D72'},
+              {text: (Math.round(item.distance / 1609 * 100) / 100) + " mi", style: {position: 'absolute', right: 5, bottom: 7 }, text100: true, color: '#A2AAAD' }
             ]}
-            contentStyle={{alignItems: 'left', paddingVertical: 5}}
+            contentStyle={{alignItems: 'left', paddingVertical: 5, color: 'white'}}
             flexG
             marginL-10
             />
         </Card>
     } />
 
-    return <View><Text text50H marginL-10 marginB-10>Rawdogging with The Tank</Text>{ output }</View>
+    return <View>
+      <Text color={'#FF5910'} text50H marginL-10 marginB-5 textAlign="center" justifyContent="center" alignItems="center">
+        <Image source={require('../assets/hotdog.png')} width={26} height={36} alignItems="center" justifyContent="center" /> Rawdogging</Text>
+        { output }
+    </View>
   } // generateFlastList()
 
   return (
-    <View style={styles.container}>
-      <SafeAreaView>
+    <View style={styles.container} flex>
+      <Stack.Screen
+        options={{
+          // https://reactnavigation.org/docs/headers#setting-the-header-title
+          title: 'Rawdogging',
+          // https://reactnavigation.org/docs/headers#adjusting-header-styles
+          headerStyle: { backgroundColor: 'green' },
+          headerTintColor: 'red',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          // https://reactnavigation.org/docs/headers#replacing-the-title-with-a-custom-component
+          // headerTitle: props => <LogoTitle {...props} />,
+          // headerTitle: props => <Text>Rawdogging</Text>,
+        }}
+      />
+      <View flex style={{width: '100%', alignItems: 'center', paddingTop: insets.top}}>
         { generateFlatList(placesList) }
-      </SafeAreaView>
-      <StatusBar style="auto" />
+        {/* <BlurView style={{width: '100%', height: 300, overflow: 'hidden', padding: 20, zIndex: 99, position: 'absolute', bottom:0 }} /> */}
+        {/* <LinearGradient style={{position: 'absolute', bottom: 0, width: '100%', height: 40}} colors={['rgba(0,45,114,0)', 'rgba(0,45,114,1)']} /> */}
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -82,11 +109,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // backgroundColor: '#e00043',
-    backgroundColor: '#e00043',
+    // fcd492
+    // e00043
+    backgroundColor: '#002D72',
     alignItems: 'center',
     justifyContent: 'center',
     color: '#fff',
   },
+  // blurContainer: {
+  //   flex: 1,
+  //   padding: 20,
+  //   margin: 0,
+  //   textAlign: 'center',
+  //   justifyContent: 'center',
+  //   overflow: 'hidden',
+  //   borderRadius: 20,
+  // },
   image: { width: '30%'},
   border: {},
 });
